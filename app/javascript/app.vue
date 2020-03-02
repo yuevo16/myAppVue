14e86b6f411b44b96698e5ca47db93af5b2bf389
yuya <yuevo16@gmail.com>
2020-03-02 19:07:45 +0900

    サンプルイベント、関数テスト

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,34 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <ol>
+      <li v-for="item in list">{{ item }}</li>
+    </ol>
+    <input v-model="form">
+    <button v-on:click="handleClick">Click</button>
+    <button v-on:click="show=!show">切り替え</button>
+    <transition>
+      <p v-if="show">Helowwww</p>
+    </transition>
   </div>
+
+
 </template>
 
 <script>
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      list: ['りんご', 'ばなな', 'いちご'],
+      form: "hey!",
+      show: true
+    }
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target)
+      window.alert('テス')
     }
   }
 }
@@ -18,5 +38,13 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+
+.v-enter-active, .v-leave-active {
+  transition: opacity 1s;
+}
+
+.v-enter, .v-leave-to {
+  opacity: 0;
 }
 </style>
