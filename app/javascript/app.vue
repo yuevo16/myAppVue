1b61f20147c8240e49048119d32679597a287691
yuya <yuevo16@gmail.com>
2020-03-03 21:54:49 +0900

    基礎問題練習

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,26 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <p>{{ message.value }}</p>
+    <p>{{ message.value.length  }}</p>
+    <p>{{ 1 + 1 }}</p>
     <ol>
       <li v-for="item in list">{{ item }}</li>
     </ol>
+    <p>{{ list[2]  }}</p>
+    <p>{{ list[num]  }}</p>
+    <input type="text" :value="message">
+    <div v-bind:text-content.prop="message">あああ</div>
+    <div v-bind:scroll-top.prop="scroll">良いい</div>
+    <p>{{ count }}回クリックしたよ！</p>
+    <button v-on:click="increment">カウントを増やす</button>
     <input v-model="form">
     <button v-on:click="handleClick">Click</button>
     <button v-on:click="show=!show">切り替え</button>
     <transition>
       <p v-if="show">Helowwww</p>
     </transition>
+    <p v-bind:class="classObject">Text</p>
+    <p v-bind:style="styleObject">Text</p>
   </div>
 
 
@@ -19,16 +30,35 @@
 export default {
   data: function () {
     return {
-      message: "Hello Vue!",
+      message: {
+        value: "Hello Vue!"
+      },
+      scroll: 0,
       list: ['りんご', 'ばなな', 'いちご'],
+      num: 1,
       form: "hey!",
-      show: true
+      show: true,
+      count: 0,
+      classObject: {
+        child: true,
+        'is-active': false
+      },
+      styleObject: {
+        color: 'red',
+        backgroundColor: 'lightgray'
+      }
     }
+  },
+  mounted: function() {
+    this.scroll = 100
   },
   methods: {
     handleClick: function(event) {
       alert(event.target)
       window.alert('テス')
+    },
+    increment: function() {
+      this.count += 1
     }
   }
 }
